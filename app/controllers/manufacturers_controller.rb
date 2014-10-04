6aea363631bff6ad1bde0255968c382da8b6dc80
Nick McLaughlin <mclaughlin.nick@gmail.com>
2014-10-03 20:01:31 -0400

    first test passing figured out factory girl

--- app/controllers/manufacturers_controller.rb
+++ app/controllers/manufacturers_controller.rb
@@ -1,2 +1,21 @@
 class ManufacturersController < ActionController::Base
+  def new
+    @manufacturer = Manufacturer.new
+  end
+
+  def create
+    @manufacturer = Manufacturer.new(manufacturer_params)
+
+    if @manufacturer.save
+      flash[:notice] = "You successfully created a manufacturer."
+      redirect_to root_path
+    else
+      render 'new'
+    end
+  end
+
+  private
+  def manufacturer_params
+    params.require(:manufacturer).permit(:name, :country)
+  end
 end
